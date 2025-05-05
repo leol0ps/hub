483f58ca22537c95c16c272a6421732794493fcc
leol0ps <leotimao2@hotmail.com>
2025-05-05 17:19:51 -0300

    test

--- contest.spec.ts
+++ contest.spec.ts
@@ -104,53 +104,53 @@ test('test', async ({ page }) => {
     await page.goto('http://localhost:8000/boca/team/run.php');
 
   }
-  // try {
-  //   await page.waitForSelector('text="NO - Contact staff" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Contact staff" on the page.`);
-  // } catch (error) {
+  try {
+    await page.waitForSelector('text="NO - Contact staff" ', { timeout: 1000 });
+    console.log(`Found "NO - Contact staff" on the page.`);
+  } catch (error) {
   
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Wrong answer" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Wrong answer" on the page.`);
-  // } catch (error) {
-  
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Compilation error" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Compilation error" on the page.`);
-  // } catch (error) {
-  // }
-  // try {
-  //   await page.waitForSelector('text="YES" ', { timeout: 1000 });
-  //   console.log(`Found "YES" on the page.`);
-  // } catch (error) {
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Time limit exceeded" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Time limit exceeded" on the page.`);
-  // } catch (error) {
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Runtime error" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Runtime error" on the page.`);
-  // } catch (error) {
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Presentation error" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Presentation error" on the page.`);
-  // } catch (error) {
-  // }
-
-
-  for (const selector of selectors) {
-    try {
-      await page.waitForSelector(`text="${selector}"`, { timeout: 1000 });
-      console.log(`Found "${selector}" on the page.`);
-    } catch (error) {
-      console.log(`"${selector}" not found on the page within timeout.`);
-    }
   }
+  try {
+    await page.waitForSelector('text="NO - Wrong answer" ', { timeout: 1000 });
+    console.log(`Found "NO - Wrong answer" on the page.`);
+  } catch (error) {
+  
+  }
+  try {
+    await page.waitForSelector('text="NO - Compilation error" ', { timeout: 1000 });
+    console.log(`Found "NO - Compilation error" on the page.`);
+  } catch (error) {
+  }
+  try {
+    await page.waitForSelector('text="YES" ', { timeout: 1000 });
+    console.log(`Found "YES" on the page.`);
+  } catch (error) {
+  }
+  try {
+    await page.waitForSelector('text="NO - Time limit exceeded" ', { timeout: 1000 });
+    console.log(`Found "NO - Time limit exceeded" on the page.`);
+  } catch (error) {
+  }
+  try {
+    await page.waitForSelector('text="NO - Runtime error" ', { timeout: 1000 });
+    console.log(`Found "NO - Runtime error" on the page.`);
+  } catch (error) {
+  }
+  try {
+    await page.waitForSelector('text="NO - Presentation error" ', { timeout: 1000 });
+    console.log(`Found "NO - Presentation error" on the page.`);
+  } catch (error) {
+  }
+
+
+  // for (const selector of selectors) {
+  //   try {
+  //     await page.waitForSelector(`text="${selector}"`, { timeout: 1000 });
+  //     console.log(`Found "${selector}" on the page.`);
+  //   } catch (error) {
+  //     console.log(`"${selector}" not found on the page within timeout.`);
+  //   }
+  // }
   
 
   await page.getByRole('link', { name: 'Logout' }).click();
