7e3083cc121e240fbe9aaa1ee0eabf6ec094d713
neptacc <164110331+neptacc@users.noreply.github.com>
2025-07-14 18:15:48 -0300

    Update contest.spec.ts

--- contest.spec.ts
+++ contest.spec.ts
@@ -1,170 +1,183 @@
-import { test, expect } from '@playwright/test';
-import { defineConfig } from '@playwright/test';
+import { test } from '@playwright/test';
 
-export default defineConfig({
-  timeout: 5 * 60 * 2000,
-});
-const now = new Date();
-//const brasilData =  new Date(now.getTime() - 3 * 60 * 60 * 1000); // github roda as actions em gmt +0 e o boca por padrão é gtm +3, portanto subtraimos 3 hrs
-//const hours = brasilData.getUTCHours();
-//const minutes = brasilData.getUTCMinutes();
-const hours = now.getHours() - 3;
-const minutes = now.getMinutes() - 3;
-var check = "Not answered yet"
-var tst = true
-const selectors = ['YES', 'NO - Compilation error', 'NO - Runtime error', 'NO - Time limit exceeded', 'NO - Presentation error', 'NO - Wrong answer', 'NO - Contact staff']
-test('test', async ({ page }) => {
-  test.setTimeout(150_000);
+test('Submit problem and configure contest', async ({ page }) => {
+  const now = new Date();
+  const brasilData = new Date(now.getTime() - 3.5 * 60 * 60 * 1000);
+  const hours = brasilData.getUTCHours();
+  const minutes = brasilData.getUTCMinutes() ;
+
   await page.goto('http://localhost:8000/boca/');
-  await page.locator('input[name="name"]').click();
   await page.locator('input[name="name"]').fill('system');
-  await page.locator('input[name="password"]').click();
   await page.locator('input[name="password"]').fill('boca');
   await page.getByRole('button', { name: 'Login' }).click();
+
   await page.getByRole('link', { name: 'Contest' }).click();
   await page.getByRole('combobox').selectOption('new');
-  await page.locator('input[name="startdateh"]').click();
   await page.locator('input[name="startdateh"]').fill(String(hours));
-  await page.locator('input[name="startdatemin"]').click();
   await page.locator('input[name="startdatemin"]').fill(String(minutes));
-  page.once('dialog', async dialog => {
-    //console.log(`Dialog message: ${dialog.message()}`);
-    await dialog.accept();
-
-  });
+  page.once('dialog', async dialog => await dialog.accept());
   await page.getByRole('button', { name: 'Activate' }).click();
 
   await page.goto('http://localhost:8000/boca/index.php');
   await page.locator('input[name="name"]').fill('admin');
-  await page.locator('input[name="name"]').press('Tab');
   await page.locator('input[name="password"]').fill('boca');
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Problems' }).click();
-  await page.locator('input[name="problemnumber"]').click();
-  await page.locator('input[name="problemnumber"]').fill('1');
-  await page.locator('input[name="problemname"]').click();
-  await page.locator('input[name="problemname"]').fill('L1_2');
-  await page.locator('input[name="probleminput"]').click();
-  await page.locator('input[name="probleminput"]').setInputFiles('L1_2.zip');
-  await page.screenshot({ path: 'screenshot.png' });
-  page.once('dialog', async dialog => {
-    //console.log(`Dialog message: ${dialog.message()}`);
-    await dialog.accept();
 
-  });
-  await page.getByRole('button', { name: 'Send' }).click();
   await page.getByRole('link', { name: 'Problems' }).click();
-  await page.screenshot({ path: 'problem.png' });
+  await page.locator('input[name="problemnumber"]').fill('1');
+  await page.locator('input[name="problemname"]').fill('L1_2');
+  await page.locator('input[name="probleminput"]').setInputFiles('L1_2.zip');
+  page.once('dialog', async dialog => await dialog.accept());
+  await page.getByRole('button', { name: 'Send' }).click();
+
   await page.getByRole('link', { name: 'Users' }).click();
-  await page.locator('input[name="importfile"]').click();
   await page.locator('input[name="importfile"]').setInputFiles('user.txt');
-  page.once('dialog', async dialog => {
-    //console.log(`Dialog message: ${dialog.message()}`);
-    await dialog.accept();
-  });
+  page.once('dialog', async dialog => await dialog.accept());
   await page.getByRole('button', { name: 'Import' }).click();
+
   await page.getByRole('link', { name: 'Site' }).click();
-  await page.locator('input[name="startdateh"]').click();
   await page.locator('input[name="startdateh"]').fill(String(hours));
-  await page.locator('input[name="startdatemin"]').click();
   await page.locator('input[name="startdatemin"]').fill(String(minutes));
   await page.getByRole('cell', { name: '<- experimental' }).click();
   await page.locator('input[name="autojudge"]').check();
-  await page.screenshot({ path: 'startcontest.png' });
-  page.once('dialog', async dialog => {
-    //console.log(`Dialog message: ${dialog.message()}`);
-    await dialog.accept();
-  });
+  page.once('dialog', async dialog => await dialog.accept());
   await page.getByRole('button', { name: 'Send' }).click();
-  await page.screenshot({ path: 'checkcontest.png' });
-  await page.goto('http://localhost:8000/boca/index.php');
-  await page.locator('input[name="name"]').click();
-  await page.locator('input[name="name"]').fill('bot');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Problems' }).click();
-  await page.screenshot({ path: 'userproblem.png' });
-  await page.getByRole('cell', { name: 'Runs' }).click();
-  await page.waitForTimeout(1000); 
-  await page.locator('select[name="problem"]').selectOption('1');
-  await page.locator('select[name="language"]').selectOption('1');
-  await page.locator('input[name="sourcefile"]').click();
-  await page.locator('input[name="sourcefile"]').setInputFiles('ans.c');
-  page.once('dialog', async dialog => {
-    //console.log(`Dialog message: ${dialog.message()}`);
-    await dialog.accept();
-  });
-  await page.getByRole('button', { name: 'Send' }).click();
-  await page.goto('http://localhost:8000/boca/team/run.php');
-  while (tst) {
-    
-    
-    try {
-      const z = await page.waitForSelector('text="Not answered yet"', { timeout: 2000 }) //locator('Not answered yet').first();
-    } catch (error) {
-      tst = false
-    }
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    await page.goto('http://localhost:8000/boca/team/run.php');
-
-  }
-  // try {
-  //   await page.waitForSelector('text="NO - Contact staff" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Contact staff" on the page.`);
-  // } catch (error) {
-  
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Wrong answer" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Wrong answer" on the page.`);
-  // } catch (error) {
-  
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Compilation error" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Compilation error" on the page.`);
-  // } catch (error) {
-  // }
-  // try {
-  //   await page.waitForSelector('text="YES" ', { timeout: 1000 });
-  //   console.log(`Found "YES" on the page.`);
-  // } catch (error) {
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Time limit exceeded" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Time limit exceeded" on the page.`);
-  // } catch (error) {
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Runtime error" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Runtime error" on the page.`);
-  // } catch (error) {
-  // }
-  // try {
-  //   await page.waitForSelector('text="NO - Presentation error" ', { timeout: 1000 });
-  //   console.log(`Found "NO - Presentation error" on the page.`);
-  // } catch (error) {
-  // }
-
-
-  for (const selector of selectors) {
-    try {
-      await page.waitForSelector(`text="${selector}"`, { timeout: 1000 });
-      console.log(`Found "${selector}" on the page.`);
-    } catch (error) {
-      console.log(`"${selector}" not found on the page within timeout.`);
-    }
-  }
-  
-
-  await page.getByRole('link', { name: 'Logout' }).click();
 });
+test('Submit solution and check result', async ({ page }) => {
+    test.setTimeout(120_000);
+    const now = new Date();
+    const brasilData = new Date(now.getTime() - 3.5 * 60 * 60 * 1000);
+    const hours = brasilData.getUTCHours();
+    const minutes = brasilData.getUTCMinutes() ;
+    const selectors = [
+      'YES',
+      'NO - Compilation error',
+      'NO - Runtime error',
+      'NO - Time limit exceeded',
+      'NO - Presentation error',
+      'NO - Wrong answer',
+      'NO - Contact staff'
+    ];
+
+
+    await page.goto('http://localhost:8000/boca/');
+    await page.locator('input[name="name"]').fill('bot');
+    await page.locator('input[name="password"]').fill('boca');
+    await page.getByRole('button', { name: 'Login' }).click();
+  
+    await page.getByRole('link', { name: 'Problems' }).click();
+    await page.getByRole('cell', { name: 'Runs' }).click();
+    await page.goto('http://localhost:8000/boca/index.php');
+    await page.locator('input[name="name"]').fill('admin');
+    await page.locator('input[name="password"]').fill('boca');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('link', { name: 'Contest' }).click();
+    await page.locator('input[name="startdateh"]').fill(String(hours));
+    await page.locator('input[name="startdatemin"]').fill('00');
+    page.once('dialog', async dialog => await dialog.accept());
+    await page.getByRole('button', { name: 'Update Contest and All Sites' }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await page.goto('http://localhost:8000/boca/');
+    await page.locator('input[name="name"]').fill('bot');
+    await page.locator('input[name="password"]').fill('boca');
+    await page.getByRole('button', { name: 'Login' }).click();
+  
+    await page.getByRole('link', { name: 'Problems' }).click();
+    await page.getByRole('cell', { name: 'Runs' }).click();
+    await page.waitForTimeout(1000);
+    await page.goto('http://localhost:8000/boca/team/run.php');
+    await page.getByRole('link', { name: 'Logout' }).click();
+  
+    
+    await page.goto('http://localhost:8000/boca/');
+    await page.locator('input[name="name"]').fill('bot');
+    await page.locator('input[name="password"]').fill('boca');
+    await page.getByRole('button', { name: 'Login' }).click();
+  
+    await page.getByRole('link', { name: 'Problems' }).click();
+    await page.getByRole('cell', { name: 'Runs' }).click();
+    
+
+    let problemOptionVisible = false;
+    const maxRetries = 50;
+    let retries = 0;
+
+    while (!problemOptionVisible && retries < maxRetries) {
+      console.log(`Retry attempt: ${retries + 1}`);
+
+      
+      await page.goto('http://localhost:8000/boca/index.php');
+
+      
+      await page.locator('input[name="name"]').fill('bot');
+      await page.locator('input[name="password"]').fill('boca');
+      await page.getByRole('button', { name: 'Login' }).click();
+
+     
+      await page.getByRole('link', { name: 'Problems' }).click();
+      await page.getByRole('cell', { name: 'Runs' }).click();
+
+      
+      try {
+        await page.locator('select[name="problem"]').selectOption('1', { timeout: 5000 })
+        //await page.waitForSelector('select[name="problem"] > option[value="1"]', { timeout: 5000 });
+        problemOptionVisible = true;
+        console.log('Problem option found!');
+      } catch (error) {
+        console.log('Problem option not found, retrying...');
+        // Logout before retrying
+        try {
+          await page.getByRole('link', { name: 'Logout' }).click();
+        } catch {
+          console.log('Logout failed or already logged out');
+        }
+        await page.waitForTimeout(3000); // wait before retrying
+        retries++;
+      }
+    }
+
+    if (!problemOptionVisible) {
+      throw new Error('Failed to find problem option after multiple retries');
+    }
+
+    
 
 
 
-// 1 YES
-// 2 NO - Compilation error
-// 3 NO - Runtime error
-// 4 NO - Time limit exceeded
-// 5 NO - Presentation error
-// 6 NO - Wrong answer
-// 7 NO - Contact staff
+
+
+
+    await page.locator('select[name="problem"]').selectOption('1');
+    await page.locator('select[name="language"]').selectOption('1');
+    await page.locator('input[name="sourcefile"]').setInputFiles('ans.c');
+  
+    
+    page.once('dialog', async dialog => await dialog.accept());
+    await page.getByRole('button', { name: 'Send' }).click();
+  
+    await page.goto('http://localhost:8000/boca/team/run.php');
+  
+    
+    let stillWaiting = true;
+    while (stillWaiting) {
+      try {
+        await page.waitForSelector('text="Not answered yet"', { timeout: 2000 });
+      } catch (error) {
+        stillWaiting = false;
+      }
+      await page.waitForTimeout(3000);
+      await page.goto('http://localhost:8000/boca/team/run.php');
+    }
+  
+    for (const selector of selectors) {
+      try {
+        await page.waitForSelector(`text="${selector}"`, { timeout: 500 });
+        console.log(`Found "${selector}"`);
+      } catch (error) {
+        console.log(`"${selector}" not found`);
+      }
+    }
+  
+    await page.getByRole('link', { name: 'Logout' }).click();
+  });
+  
